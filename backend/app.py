72376f7b638f04ca0eee77b428d0e54518dcd52d
Sahil Alamgir <sahil.alamgir@gmail.com>
2025-01-25 15:33:59 -0500

    Add API route to GPT to gauge experience level of user regarding stocks

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import numpy as np
 import pandas as pd
 from pandas_datareader import data, wb
@@ -6,13 +7,22 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import requests
 import datetime as dt
+import 
+# import func from om_gpt
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/api/etf")
-def get_etfs():
-    
+def get_etf():
+    pass
+
+@app.route("/api/gpt")
+def get_level():
+    data = request.get_json()
+    response = data.get("response")
+    level = func(response)
+    return jsonify({"level": level})
 
 
 if __name__ == "__main__":
