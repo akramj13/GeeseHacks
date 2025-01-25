d019e1c6dc26902cf82f9ca24c06e78317b9ee28
Sahil Alamgir <sahil.alamgir@gmail.com>
2025-01-25 15:53:27 -0500

    Add route to get percent change and chart on stock data

--- backend/app.py
+++ backend/app.py
@@ -7,15 +7,20 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import requests
 import datetime as dt
-import 
+from stock_handler import get_stock_data 
 # import func from om_gpt
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/api/etf")
-def get_etf():
-    pass
+@app.route("/api/stock")
+def get_stock():
+    ticker = request.params.get("ticker")
+    start = request.params.get("start")
+    end = request.params.get("end")
+
+    stock_data = get_stock_data(ticker, start, end)
+    return jsonify({"percent_change": data["percent_change"], "chart": data["chart"]})
 
 @app.route("/api/gpt")
 def get_level():
