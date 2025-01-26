054d532c2a023ae94b083dd883560fb516324b92
akramj13 <125495000+akramj13@users.noreply.github.com>
2025-01-26 02:54:50 -0500

    update index

--- frontend/data/index.ts
+++ frontend/data/index.ts
@@ -1,23 +1,38 @@
 export const navItems = [
     { label: 'Investments', link: '/' },
     { label: 'Insurance', link: '/' },
-    { label: 'LearnETF', link: '/learnetf' },
+    { label: 'Securities Educator', link: '/learnetf' },
 ];
 
 export const modules = [
     {
       slug: "module-1",
-      title: "Module 1: Intro to Stocks",
-      description: "Learn the basics of artificial intelligence and its applications.",
+      level: "Hello",
+      title: "Module 1: A Summary of Stocks",
+      description: "A stock (also called a share) represents partial ownership in a company. When you purchase stock, you become a shareholder, meaning you own a fraction of that company. If the company performs well, its value increases, and the price of your stock might go up. However, if the company struggles or loses value, the price of your stock can decrease. Stocks are usually bought through brokers on stock exchanges like the New York Stock Exchange (NYSE), New Toronto Stock Exchange (TSX) or NASDAQ.",
     },
     {
       slug: "module-2",
-      title: "Module 2: Intro to ETFs",
-      description: "Dive into NLP concepts and learn how machines process human language.",
+      level: "Beginner",
+      title: "Module 2: Summary of Bonds",
+      description: "A bond is essentially a loan made by an investor to a company or government. The issuer (borrower) agrees to pay back the amount borrowed (the principal) at a later date, called the maturity date, along with periodic interest payments, known as coupons. Bonds are considered safer than stocks, but they offer lower returns. This is due to the fact that bonds are less volatile and their value is more predictable.",
     },
     {
       slug: "module-3",
-      title: "Module 3: Intro to Bonds",
-      description: "Explore machine learning models and how they are trained.",
+      level: "Beginner",
+      title: "Module 3: Summary of Mutual Funds",
+      description: "A mutual fund is a pool of money from many investors that is managed by professionals. The fund manager buys and sells investments like stocks or bonds on behalf of all the investors in the fund. When you invest in a mutual fund, you own shares of the fund, and the value of those shares goes up or down based on the performance of the investments inside the fund.",
+    },
+    {
+      slug: "module-4",
+      level: "Beginner",
+      title: "Module 4: Summary of ETFs (Exchange-Traded Funds)",
+      description: "An ETF is a type of investment fund that holds a collection of different assets, like stocks or bonds, and you can buy shares of the ETF, just like a stock. ETFs are designed to track the performance of an index (e.g., S&P 500) or a specific sector. They’re a way for investors to diversify without buying individual stocks or bonds.",
+    },
+    {
+      slug: "module-5",
+      level: "Beginner",
+      title: "Module 5: Summary of Segregated Funds",
+      description: "An ETF is a type of investment fund that holds a collection of different assets, like stocks or bonds, and you can buy shares of the ETF, just like a stock. ETFs are designed to track the performance of an index (e.g., S&P 500) or a specific sector. They’re a way for investors to diversify without buying individual stocks or bonds.",
     },
   ];
