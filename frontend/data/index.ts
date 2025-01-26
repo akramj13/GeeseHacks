7e721d24431adfac2d4d83ffe414d93533f248e4
Sahil Alamgir <133982347+sahilalamgir@users.noreply.github.com>
2025-01-26 00:36:40 -0500

    Fix spelling of modules

--- frontend/data/index.ts
+++ frontend/data/index.ts
@@ -7,17 +7,17 @@ export const navItems = [
 export const modules = [
     {
       slug: "module-1",
-      title: "Module 1: Intro to stocks",
+      title: "Module 1: Intro to Stocks",
       description: "Learn the basics of artificial intelligence and its applications.",
     },
     {
       slug: "module-2",
-      title: "Module 2: Intro to etfs",
+      title: "Module 2: Intro to ETFs",
       description: "Dive into NLP concepts and learn how machines process human language.",
     },
     {
       slug: "module-3",
-      title: "Module 3: intro to bonds",
+      title: "Module 3: Intro to Bonds",
       description: "Explore machine learning models and how they are trained.",
     },
   ];
