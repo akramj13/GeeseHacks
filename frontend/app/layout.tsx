8a3a7d2baf19f98fb02c97f5692906704f6431e7
akramj13 <125495000+akramj13@users.noreply.github.com>
2025-01-26 04:21:58 -0500

    update

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -29,25 +29,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <LevelProvider>{children}</LevelProvider>
-<<<<<<< Updated upstream
-        {/* <script type="text/javascript">
-  (function(d, t) {
-      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
-      v.onload = function() {
-        window.voiceflow.chat.load({
-          verify: { projectID: 'YOUR_PROJECT_ID' },
-          url: 'https://general-runtime.voiceflow.com',
-          versionID: 'production'
-        });
-      };
-      v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; 
-      v.type = "text/javascript"; 
-      s.parentNode.insertBefore(v, s);
-  })(document, 'script');
-</script> */}
-
-=======
->>>>>>> Stashed changes
       </body>
     </html>
   );
